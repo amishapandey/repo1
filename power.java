e978cca77ea891698993bcad3c56708d7b5f6b8b
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-02-20 20:37:29 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.lang.Math;
+class Simple{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int k=sc.nextInt();
+int s;
+s=(int)Math.pow(n,k);
+System.out.println(s);
+}
+}
