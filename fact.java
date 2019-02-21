54882d9f552c430b9a745be84084580281ab3dfa
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-02-21 23:09:22 +0530

    Update fact.java

--- fact.java
+++ fact.java
@@ -4,8 +4,8 @@ public static void main(String args[]){
 Scanner scc=new Scanner(System.in);
 int n=sc.nextInt();
 int c,i,sum=0;
-for(i=n;i<=1;i--){
-c=i*i-1;
+for(i=n;i>=1;i--){
+c=i*(i-1);
 sum=sum+c;
 }
 System.out.println(sum);
