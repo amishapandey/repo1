8cad9e5443c6abb7510651465a79469e69926963
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-02-24 19:20:38 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,16 +1,18 @@
-import java.util.Scanner;
+import java.util.*;
 class Simple{
 public static void main(String args[]){
 Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
 int i,c=0;
-for(i=1;i<=9;i++){
+for(i=2;i<Math.sqrt(num);i++){
 if(n%i==0){
-c=c+1;
+c+=1;
 }
 }
-if(n>2){
-System.out.println("yes");
+if(c>2){
+System.out.println("no");
 }
+  else
+    System.out.println("yes");
 }
 }
