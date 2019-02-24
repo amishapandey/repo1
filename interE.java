0d73ece1e2b048bb527c1569795d73401062f778
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-02-24 19:42:25 +0530

    Create interE.java

---
+++ interE.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Simple{
+public static void main(String args[]){
+Scanner n=new Scanner(System.in);
+int k=n.nextInt();
+int l=n.nextInt();
+int i;
+for(i=k+1;i<l;i++){
+if(i%2==0){
+System.out.print(i+" ");
+}
+}
+}
+}
