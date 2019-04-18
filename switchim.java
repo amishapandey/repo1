c08c833da8fd41f7460438423a83abcfed828f14
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-18 23:19:08 +0530

    Create switchim.java

---
+++ switchim.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    switch(n)
+	    {
+	        case 1:
+	            
+	           System.out.println("One");
+	           break;
+	       case 2:
+	            
+	           System.out.println("two");
+	           break;
+	       case 3:
+	            
+	           System.out.println("three");
+	           break;
+	       case 4:
+	            
+	           System.out.println("four");
+	           break;
+	       case 5:
+	           System.out.println("five");
+	           break;
+	       case 6:
+	            
+	           System.out.println("six");
+	           break;
+	       default:
+	       System.out.println("wrong choice");
+	    }   
+	}
+}
